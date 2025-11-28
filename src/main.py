dd2130f5525c3b72e65fe59bb086b5e912b4b390
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-11-29 01:16:46 +0300

    added an arbituary func

--- src/main.py
+++ src/main.py
@@ -3,3 +3,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+def CNN_function(matrix): # this is called by the server. It takes a matrix as input and returns a confidence score. Feel free to change -Deniz & Gün
+    # some function here bro idk
+    return "CNN result"
