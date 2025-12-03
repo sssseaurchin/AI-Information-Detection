f3b4caa639c109468ffda7905e5fa8307979280d
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-12-03 17:12:54 +0300

    Moved servel call function template -Rüzgar

--- src/cnnModel.py
+++ src/cnnModel.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import numpy as np
+import pandas as pd
 
+
+
+# Server call template
+# TODO
+def returnConfidence(confidence: float) -> float:
+    return confidence
+    
