e8a28b803bea7fcf506e80dc47d34bdb6e0e70da
berk4tes <berkates48@outlook.com>
2026-01-06 01:46:08 +0300

    commit 123

--- src/cnn/services.py
+++ src/cnn/services.py
@@ -1,16 +1,48 @@
+import os
+# --- DİKKAT: BU SATIRLAR EN TEPEDE OLMAK ZORUNDA ---
+# Başka hiçbir import yapmadan önce bunu ayarlamalıyız.
+os.environ["TF_USE_LEGACY_KERAS"] = "1"
+# ---------------------------------------------------
+
+# ŞİMDİ diğerlerini çağırabiliriz
 from cnn import cnnModel
 import tensorflow as tf
-import os
 
-def cnn_analyze_image(image_name:str):
+"""def cnn_analyze_image(image_name:str):
     path = os.path.dirname(os.path.abspath(__file__))
     MODEL_PATH = os.path.join(path, "model", "ai_detection_model.h5")
-    image_name = path + "\\Model\\" + image_name
-    
+    # path'i str() içine alıyoruz
+    image_name = str(path) + "\\Model\\" + image_name    
     model = tf.keras.models.load_model(MODEL_PATH, compile=False)
     prediction = cnnModel.predict_image(model, image_name)
 
-    print(f"Prediction for image {image_name}: {prediction}")
+    print(f"Prediction for image {image_name}: {prediction}")"""
+
+MODEL_NAME = "model.h5"
+
+def cnn_analyze_image(image_path_input):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    MODEL_PATH = os.path.join(current_dir, "model", MODEL_NAME) 
+    
+    final_image_path = str(image_path_input)
+
+    print(f"DEBUG: Model Yolu: {MODEL_PATH}")
+    print(f"DEBUG: Resim Yolu: {final_image_path}")
+
+    try:
+        model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+        print("MODEL YUKLENDI")
+        prediction = cnnModel.predict_image(model, final_image_path)
+        
+        print(f"Prediction result: {prediction}")
+        
+        return prediction
+    except Exception as e:
+        print(f"HATA (CNN Analiz): {e}")
+        # Hata durumunda -1 veya hata mesajı dönebilirsin
+        return -1
+    
 
 if __name__ == "__main__": 
     cnn_analyze_image(r"aaa.jpg")
