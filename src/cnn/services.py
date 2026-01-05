59306e661f50da132da868c25b5a3eb657d21158
Ruzgar Erdem <ruzgarerdem@gmail.com>
2026-01-05 20:55:50 +0300

    Fixed services.py

--- src/cnn/services.py
+++ src/cnn/services.py
@@ -2,14 +2,16 @@ import cnnModel
 import tensorflow as tf
 import os
 
-def cnn_analyze_image(image_path:str):
+def cnn_analyze_image(image_name:str):
     path = os.path.dirname(os.path.abspath(__file__))
     MODEL_PATH = os.path.join(path, "model", "ai_detection_model.h5")
-    model = tf.keras.models.load_model(MODEL_PATH)
-    prediction = cnnModel.predict_image(model, image_path)
+    image_name = path + "\\Model\\" + image_name
+    
+    model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+    prediction = cnnModel.predict_image(model, image_name)
 
-    print(f"Prediction for image {image_path}: {prediction}")
+    print(f"Prediction for image {image_name}: {prediction}")
 
 if __name__ == "__main__": 
-    cnn_analyze_image(r"\model\kirkified_ruzgar.jpeg")
+    cnn_analyze_image(r"aaa.jpg")
             
