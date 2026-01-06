45836db7e357e3491dc1c84a6c063ea3a600e07e
Deniz Yalim <denizyalimyilmaz@gmail.com>
2026-01-06 08:38:03 +0300

    renamed stuff

--- src/lstm/demo.py
+++ src/lstm/demo.py
@@ -1,38 +1,24 @@
 # Artık model yükleme yok, sadece servisi çağırıyoruz!
 from services import analyze_text
-import time
-
-print("\n" + "="*50)
-print("   AI vs HUMAN - ETKİLEŞİMLİ DEMO   ")
-print("="*50)
-print("Çıkış yapmak için 'q' yazıp Enter'a basın.\n")
+import time 
 
 while True:
     try:
-        user_input = input("Metni Girin: ")
-        
-        if user_input.lower() == 'q':
-            print("Güle güle! 👋")
-            break
-            
+        user_input = input("Enter Demo Text: ")
+         
         if not user_input.strip():
             continue
 
         print("Analiz ediliyor...")
-        time.sleep(0.5) # Biraz heyecan katalım :)
         
-        # --- BÜTÜN SİHİR BURADA ---
-        skor = analyze_text(user_input)
-        # --------------------------
+        score = analyze_text(user_input)
 
-        guven = skor * 100 if skor > 0.5 else (1 - skor) * 100
-        etiket = "🤖 YAPAY ZEKA (AI)" if skor > 0.5 else "👤 İNSAN (HUMAN)"
+        guven = score * 100 if score > 0.5 else (1 - score) * 100
+        label = "AI" if score > 0.5 else "HUMAN"
         
-        print("-" * 40)
-        print(f"SONUÇ: {etiket}")
-        print(f"Güven Skoru: %{guven:.2f} (Ham Skor: {skor:.4f})")
-        print("-" * 40 + "\n")
+
+        print(f"SONUÇ: {label}")
+        print(f"Güven Skoru: %{guven:.2f} (Ham Skor: {score:.4f})")
 
     except KeyboardInterrupt:
-        print("\nProgram kapatıldı.")
         break
